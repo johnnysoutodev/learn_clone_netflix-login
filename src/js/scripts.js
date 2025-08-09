3c85c065527bf5f8bfe1aee36634ef4a2c6f6739
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2025-08-09 19:53:43 -0300

    adicionando ano no rodapé

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,6 @@
 // scripts.js
+
+const ano = new Date().getFullYear();
+const year = document.getElementById("year");
+year ? year.innerHTML = `<span>${ano} JSD Technologies. Todos os direitos reservados.</span>` : null;
+
